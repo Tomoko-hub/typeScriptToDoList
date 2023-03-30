5b640724d5a0ce8a446d87149c0c5609509196a7
Tomoko-hub <haisaitomoco@gmail.com>
2023-03-30 21:19:55 +0300

    create input state and add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { FormControl, TextField } from '@material-ui/core';
 import React,{ useState, useEffect } from 'react';
 import './App.css';
-import { db } from "./firebase"
+import { db } from "./firebase";
+import { AddToPhotos } from '@material-ui/icons';
 
 const App:React.FC=()=> {
 
   const [tasks, setTasks] = useState([{id:"",title:""}]);
+  const [input, setInput] = useState("");
 
   useEffect(()=>{
     const unSub = db.collection("tasks").onSnapshot((snapshot)=>{
@@ -15,9 +18,32 @@ const App:React.FC=()=> {
     return ()=>unSub();
   },[]);
 
-  return <div className="App">
-    {tasks.map((task)=><h3>{task.title}</h3>)}
+  const newTask = (event:React.MouseEventHandler<HTMLButtonElement>)=>{
+    db.collection("task").add({title:input});
+    setInput("");
+  }
+
+  return (
+  <div className="App">
+    <h1>ToDo List by React/Firebase</h1>
+    <FormControl>
+      <TextField
+        label="New Task ?"
+        value={input}
+        onChange={(event:React.ChangeEvent<HTMLInputElement>)=>setInput(event.target.value)}
+      ></TextField>
+    </FormControl>
+    <button 
+      disabled={!input}
+      onClick={newTask}
+    >
+      <AddToPhotos />
+    </button>
+    {tasks.map((task)=>(
+    <h3 key={task.id}>{task.title}</h3>
+    ))}
   </div>
+  )
 };
 
 export default App;
